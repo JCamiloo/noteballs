8cb1be823c2e7cb31bbb10127c2d82520f917416
JCamiloo <jcamilo.osorio15@gmail.com>
2022-05-12 09:19:48 -0500

    adding methods, child components, computed & emit

--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -3,30 +3,53 @@
     <div class="card has-background-success-dark p-4 mb-5">
       <div class="field">
         <div class="control">
-          <textarea class="textarea" placeholder="Add a new note" />
+          <textarea v-model="newNote" class="textarea" placeholder="Add a new note" ref="newNoteRef" />
         </div>
       </div>
 
       <div class="field is-grouped is-grouped-right">
         <div class="control">
-          <button class="button is-link has-background-success">Add New Note</button>
+          <button class="button is-link has-background-success" @click="addNote" :disabled="!newNote">
+            Add New Note
+          </button>
         </div>
       </div>
     </div>
 
-    <div class="card mb-4" v-for="i in 3">
-      <div class="card-content">
-        <div class="content">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim nesciunt quam laborum quisquam fugiat labore
-          ducimus architecto libero unde facere illo iure, aliquid repellat quasi excepturi ab omnis laudantium
-          asperiores?
-        </div>
-      </div>
-
-      <footer class="card-footer">
-        <a class="card-footer-item">Edit</a>
-        <a class="card-footer-item">Delete</a>
-      </footer>
-    </div>
+    <Note v-for="note in notes" :key="note.id" :note="note" @deleteNote="deleteNote" />
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import Note from '@/components/notes/Note.vue'
+
+const newNote = ref('')
+const newNoteRef = ref(null)
+const notes = ref([
+  {
+    id: 1,
+    content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim nesciunt quam laborum quisquam fugiat labore"
+  },
+  {
+    id: 2,
+    content: "Shorter note"
+  }
+])
+
+const addNote = () => {
+  const note = {
+    id: new Date().getTime(),
+    content: newNote.value
+  }
+
+  notes.value.unshift(note)
+  newNote.value = ''
+  newNoteRef.value.focus()
+}
+
+const deleteNote = (id) => {
+  notes.value = notes.value.filter(note => note.id !== id)
+}
+</script>
+
