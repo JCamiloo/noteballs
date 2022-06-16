9228e0db80bde607802b708d887200ff34606c27
JCamiloo <jcamilo.osorio15@gmail.com>
2022-06-15 22:13:08 -0500

    add props for a reusable component

--- src/views/EditNoteView.vue
+++ src/views/EditNoteView.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="edit-note">
-    <h1>Edit Note with an id of: {{ $route.params.id }}</h1>
+    <AddEditNote
+      v-model="noteContent"
+      bgColor="link"
+      placeholder="Edit note"
+      label="Edit Note"
+      ref="addEditNoteRef"
+    >
+      <template #buttons>
+        <button
+          @click="$router.back()"
+          class="button is-link is-light"
+        >
+          Cancel
+        </button>
+        <button
+          :disabled="!noteContent"
+          class="button is-link has-background-link"
+        >
+          Save Note
+        </button>
+      </template>
+    </AddEditNote>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import AddEditNote from '../components/notes/AddEditNote.vue'
+
+const noteContent = ref('')
+</script>
